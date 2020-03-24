7bf4e724e2fb0752ad856910dbc85194491c8c43
Iván López <lopezi@objectcomputing.com>
2020-03-24 15:47:54 +0100

    Use Pojo from 'core' in 'app'

---
+++ app/src/main/java/example/micronaut/BookController.java
@@ -0,0 +1,16 @@
+package example.micronaut;
+
+import example.micronaut.pojo.Book;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+
+@Controller("/books")
+public class BookController {
+
+    @Get
+    Book index() {
+        Book book = new Book();
+        book.setName("Micronaut");
+        return book;
+    }
+}
