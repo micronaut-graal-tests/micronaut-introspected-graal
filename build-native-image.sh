529f2230b73e806e5c13211ebedd3de485aa1a20
Iván López <lopezi@objectcomputing.com>
2020-05-19 12:52:22 +0200

    Remove --static when building native-image

--- build-native-image.sh
+++ build-native-image.sh
@@ -1,2 +1,2 @@
 ./gradlew :app:assemble
-native-image --no-server --no-fallback --static --class-path app/build/libs/app-0.1-all.jar
+native-image --no-server --no-fallback --class-path app/build/libs/app-0.1-all.jar
